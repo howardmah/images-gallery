8321dcd69ec869d79c7881a9212f13d46f03f5ce
Howard Mah <howard.d.mah@gmail.com>
2022-01-31 01:29:34 -0600

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/Logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#fefefe',
 };
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '15rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
